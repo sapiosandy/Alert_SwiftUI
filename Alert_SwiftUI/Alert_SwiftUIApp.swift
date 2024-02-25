f74ab593f0bba3ed9c8212cf7b817c338688d5c8
Sandy Gomez <sgomez1029@gmail.com>
2024-02-24 18:08:16 -0800

    Initial Commit

---
+++ Alert_SwiftUI/Alert_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alert_SwiftUIApp.swift
+//  Alert_SwiftUI
+//
+//  Created by Sandra Gomez on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Alert_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
